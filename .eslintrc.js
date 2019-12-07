2e9abc362830b4c03ce9e94e7cf77850f9cee377
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2019-12-07 13:39:05 -0300

    fixed the prettier config with eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,16 @@ module.exports = {
     commonjs: true,
     es6: true
   },
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2018
   },
   rules: {
-    "prettier/prettier": ["error"]
+    'prettier/prettier': ['warn']
   }
 };
